43c997ca5a5a9fa71bd9977d4b8a6518ac40c87a
harshadamahajan18 <harshadamahajan51@gmail.com>
2023-10-31 14:57:34 +0530

    created first component and custom added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    //  this is called fragment
+    <>
+      <Chai />
+      <h1>React Demo</h1>
+      <p>Test para</p>
+    </>
+  )
+}
+
+export default App
